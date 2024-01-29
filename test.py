795e07a0bedfcbc01f6cc3f8e7d19e75d4c035a3
Yevhenii12376 <o.ionenko129@gmail.com>
2024-01-29 20:54:43 +0200

    first test

--- test.py
+++ test.py
@@ -6,12 +6,27 @@ from playwright.sync_api import Playwright, APIRequestContext
 GITHUB_API_TOKEN = "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"
 assert GITHUB_API_TOKEN, "GITHUB_API_TOKEN is not set"
 
-GITHUB_USER = ("Yevhenii12376")
+GITHUB_USER = "Yevhenii12376"
 assert GITHUB_USER, "GITHUB_USER is not set"
 
 GITHUB_REPO = "test"
 
-# ...
+@pytest.fixture(scope="session")
+def api_request_context(
+    playwright: Playwright,
+) -> Generator[APIRequestContext, None, None]:
+    headers = {
+        # We set this header per GitHub guidelines.
+        "Accept": "application/vnd.github.v3+json",
+        # Add authorization token to all requests.
+        # Assuming personal access token available in the environment.
+        "Authorization": f"token {GITHUB_API_TOKEN}",
+    }
+    request_context = playwright.request.new_context(
+        base_url="https://api.github.com", extra_http_headers=headers
+    )
+    yield request_context
+    request_context.dispose()
 
 def test_should_create_bug_report(api_request_context: APIRequestContext) -> None:
     data = {
