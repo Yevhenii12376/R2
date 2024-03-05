883a26e299589cf59d8a2bd78fffee1a1bd4a8df
oione <o.ionenko129@gmail.com>
2024-03-05 21:33:17 +0200

    upt

--- FinalProject/tk2.py
+++ FinalProject/tk2.py
@@ -1,17 +1,24 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 
-def run(playwright) -> None:
+
+def Poshuk_tovaru(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://makeup.com.ua/ua/categorys/3/")
-    page.click(".header-office")
-    page.click('a[href="/ua/register/"]')
-    page.check('input[name="roistat-checkbox-1"]')
+    page.goto("https://makeup.com.ua/ua/")
+    page.wait_for_timeout(1500)
+    page.locator(".search-button").first.click()
+    page.wait_for_timeout(1500)
+    page.get_by_placeholder("Понад 249 000 товарів").fill("крем для рук")
+    page.wait_for_timeout(1500)
+    page.get_by_placeholder("Понад 249 000 товарів").press("Enter")
+    page.wait_for_timeout(1500)
+    page.goto("https://makeup.com.ua/ua/product/32739/")
 
     # ---------------------
     context.close()
     browser.close()
 
+
 with sync_playwright() as playwright:
-    run(playwright)
+    Poshuk_tovaru(playwright)
