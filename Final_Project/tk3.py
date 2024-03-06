1c36936a6ce29e7ac76042274ffd7d0263911493
Yevhenii12376 <48484535+Yevhenii12376@users.noreply.github.com>
2024-03-06 14:01:48 +0200

    upt

--- Final_Project/tk3.py
+++ Final_Project/tk3.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-
+#
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
