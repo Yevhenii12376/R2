e4e8356820e03814515e693b744bb5c753a1dceb
Yevhenii12376 <48484535+Yevhenii12376@users.noreply.github.com>
2024-03-06 14:00:04 +0200

    tk3.py

--- FinalProject/tk3.py
+++ FinalProject/tk3.py
@@ -1,25 +1,22 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 
-def test_makeup_website():
-    with sync_playwright() as playwright:
-        # Запускаем браузер
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
 
-        # Переходим на веб-сайт makeup.com.ua
-        page.goto("https://makeup.com.ua/ua/")
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://makeup.com.ua/ua/")
+    page.wait_for_timeout(1500)
+    page.get_by_role("link", name="Тіло і ванна").click()
+    page.wait_for_timeout(1500)
+    page.locator("#popularinput-checkbox-2243-29953").click()
+    page.wait_for_timeout(1500)
+    page.goto("https://makeup.com.ua/ua/categorys/321073/#o[2243][]=29953")
 
-        # Проверяем заголовок страницы
-        assert page.title() == "MakeUp - інтернет-магазин декоративної косметики та парфумерії"
+    # ---------------------
+    context.close()
+    browser.close()
 
-        # Проверяем наличие основных элементов на странице
-        assert page.inner_text("body") != ""
 
-        # Закрываем браузер
-        context.close()
-        browser.close()
-
-# Запускаем тест
-if __name__ == "__main__":
-    test_makeup_website()
+with sync_playwright() as playwright:
+    run(playwright)
